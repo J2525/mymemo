18d5ff1856a803c856a1d52222a9134f82f8517a
J2525 <blue.east1028@gmail.com>
2019-01-02 20:30:52 +0900

    add edit.erb

--- memo.rb
+++ memo.rb
@@ -23,7 +23,25 @@ post "/" do
 end
 
 get '/show/:id' do
+  @id = params["id"].to_i
   @paraData = File.open("articles.txt").readlines
   @article_array = @paraData[params["id"].to_i].split(",")
+  erb :show
+end
+
+get '/edit/:id' do
+  @id = params["id"].to_i
+  @paraData = File.open("articles.txt").readlines
+  @article_array = @paraData[params["id"].to_i].split(",")
+  erb :edit
+end
+
+post '/update/:id' do
+  File.open("articles.txt", "w+") do |f|
+    @articles = f.read.split("\n") #ファイルの中身を配列に入れる
+    @articles[params["id"].to_i] = params[:article].split("\r\n").join(",") #要素を置き換える
+    f.puts("#{@articles}") #書き込む
+  end
+  @article_array = params[:article].split("\r\n")
   erb :show
 end
